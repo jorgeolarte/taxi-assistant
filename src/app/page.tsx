c2e0add1058530a63c3d83413cda7be36ecb34aa
Jorge Olarte <jorge.olarte@ciandt.com>
2024-05-09 14:54:19 -0500

    feat: adding navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,46 +3,23 @@ import { useChat } from "ai/react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Messages } from "@/components/message/messages";
+import { Separator } from "@/components/ui/separator";
+import Prompt from "@/components/message/prompt";
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit, isLoading } =
     useChat();
 
   return (
-    <div className='flex flex-col justify-between gap-2 py-4 px-2 w-full max-w-md m-auto h-screen'>
+    <div className='mt-14 flex flex-col justify-between gap-2 py-4 px-2 w-full max-w-md m-auto h-screen'>
       <Messages messages={messages} />
 
-      <div className='sticky bottom-0'>
-        <form
-          onSubmit={handleSubmit}
-          className='flex flex-row w-full m-auto max-w-sm items-center gap-2'
-        >
-          <Input
-            type='text'
-            placeholder='Dime, dónde hago llegar tu taxi?'
-            value={input}
-            onChange={handleInputChange}
-            disabled={isLoading}
-            className=''
-          />
-          <Button variant='outline' type='submit' disabled={isLoading}>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='h-6 w-6 text-primary'
-              viewBox='0 0 24 24'
-            >
-              <path
-                fill='none'
-                stroke='currentColor'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='1.5'
-                d='m14 10l-3 3m9.288-9.969a.535.535 0 0 1 .68.681l-5.924 16.93a.535.535 0 0 1-.994.04l-3.219-7.242a.534.534 0 0 0-.271-.271l-7.242-3.22a.535.535 0 0 1 .04-.993l16.93-5.925Z'
-              />
-            </svg>
-          </Button>
-        </form>
-      </div>
+      <Prompt
+        input={input}
+        handleInputChange={handleInputChange}
+        handleSubmit={handleSubmit}
+        isLoading={isLoading}
+      />
     </div>
   );
 }
--- src/components/message/hero.tsx
+++ src/components/message/hero.tsx
@@ -4,7 +4,10 @@ export function Hero() {
   return (
     <div className=' flex flex-col justify-center  h-screen'>
       <h1 className='text-4xl font-extrabold tracking-tigh'>Hola 👋</h1>
-      <p className='leading-7'>Soy el asistente de IA 🤖 de Coomocart</p>
+      <p className='leading-7'>Soy el asistente de IA 🤖 de Coomocart.</p>
+      <p className='leading-5 text-xs italic'>
+        * Este es un proyecto de demostración, ningun taxi será solicitado.
+      </p>
     </div>
   );
 }
