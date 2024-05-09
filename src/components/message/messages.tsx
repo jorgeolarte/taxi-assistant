9296de7629358e055f020f6afda4c48ab53a3e00
Jorge Olarte <jorge.olarte@ciandt.com>
2024-05-09 13:39:41 -0500

    fix: messages

--- src/components/message/messages.tsx
+++ src/components/message/messages.tsx
@@ -1,14 +1,16 @@
 "use client";
 import { Suspense } from "react";
-import { useChat } from "ai/react";
 import { Bubble } from "@/components/message/bubble";
 import { BubbleSkeleton } from "@/components/message/bubble-skeleton";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Hero } from "@/components/message/hero";
+import type { Message } from "ai/react";
 
-export function Messages() {
-  const { messages } = useChat();
+type Props = {
+  messages: Message[];
+};
 
+export function Messages({ messages }: Props) {
   if (messages.length === 0) return <Hero />;
 
   return (
