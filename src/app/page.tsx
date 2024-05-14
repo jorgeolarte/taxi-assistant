84cb6643bef69cb6e15d93002dd97c78ed231b9e
Jorge Olarte <jorge.olarte@ciandt.com>
2024-05-14 06:39:55 -0500

    chore: removing default export

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import Bot from "@/components/message";
+import { Bot } from "@/components/message";
 import { BotProvider } from "@/providers/BotContext";
 
 export default function HomePage() {
--- src/components/message/index.tsx
+++ src/components/message/index.tsx
@@ -2,7 +2,7 @@
 import { Messages } from "@/components/message/messages";
 import { Prompt } from "@/components/message/prompt";
 
-export default function Bot() {
+export function Bot() {
   return (
     <div className='mt-14 flex flex-col justify-between gap-2 py-4 px-2 w-full max-w-md m-auto h-screen'>
       <Messages />
